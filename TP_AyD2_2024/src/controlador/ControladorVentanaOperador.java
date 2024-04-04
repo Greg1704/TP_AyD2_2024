cefaafbeacb75a79c5a0fc52c14bb32c8edb8372
NahuelBarriga <105519579+NahuelBarriga@users.noreply.github.com>
2024-04-04 15:36:10 -0300

    dawd

---
+++ TP_AyD2_2024/src/controlador/ControladorVentanaOperador.java
@@ -0,0 +1,29 @@
+package controlador;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import Ventana.VentanaOperador;
+
+
+public class ControladorVentanaOperador implements ActionListener{
+
+	private VentanaOperador ventanaOperador; 
+	
+
+	public ControladorVentanaOperador() {
+		super();
+		this.ventanaOperador = new VentanaOperador();
+		//this.ventanaOperador.setActionListener();
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+	
+}
+
