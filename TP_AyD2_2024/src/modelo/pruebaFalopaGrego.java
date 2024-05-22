8930c5bb35fd7005f306c25dc9392c9d848e4b12
Gregorio Firmani <gregoriofirmani@gmail.com>
2024-05-22 16:28:52 -0300

    Archivo Configuración

--- TP_AyD2_2024/src/modelo/pruebaFalopaGrego.java
+++ TP_AyD2_2024/src/modelo/pruebaFalopaGrego.java
@@ -1,11 +1,30 @@
 package modelo;
 
-import java.time.LocalDateTime;
-import java.time.LocalTime;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class pruebaFalopaGrego {
-	public static void main(String[] args) {
-		LocalTime fechaActual = LocalTime.now();
-		System.out.println(fechaActual.withNano(0));
-	}
+    public static void main(String[] args) {
+        // Ruta del archivo
+        String rutaArchivo = "Ejecutables/ArchivoConfiguracion.txt";
+        
+        // Variables para guardar las palabras
+        String strategy = null;
+        String persistencia = null;
+
+        try (BufferedReader br = new BufferedReader(new FileReader(rutaArchivo))) {
+            // Leer la primera línea
+            strategy = br.readLine();
+            // Leer la segunda línea
+            persistencia = br.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // Imprimir las palabras leídas
+        System.out.println("strategy: " + strategy);
+        System.out.println("persistencia: " + persistencia);
+    }
+
 }
