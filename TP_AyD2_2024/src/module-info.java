d44a95969012d029e13c3035f23b9382a90dedd7
NahuelBarriga <105519579+NahuelBarriga@users.noreply.github.com>
2024-05-24 17:00:53 -0300

    a

--- TP_AyD2_2024/src/module-info.java
+++ TP_AyD2_2024/src/module-info.java
@@ -1,12 +1,18 @@
-/**
- * 
- */
-/**
- * 
- */
 module TP_AyD2_2024 {
-	requires java.desktop;
-	requires com.fasterxml.jackson.databind;
+	exports ejecutables;
+	exports template;
+	exports interfaces;
+	exports ventana;
+	exports controlador;
+	exports abstractFactory;
+	exports state;
+	exports persistencia;
+	exports strategy;
+	exports modelo;
+
 	requires com.fasterxml.jackson.core;
+	requires com.fasterxml.jackson.databind;
 	requires com.fasterxml.jackson.dataformat.xml;
+	requires java.desktop;
+	requires java.xml;
 }
