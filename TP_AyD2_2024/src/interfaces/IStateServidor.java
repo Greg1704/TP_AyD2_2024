a313a2eb94704971721fa7fd6b9c84ad74967d5c
Gregorio Firmani <gregoriofirmani@gmail.com>
2024-05-24 16:07:12 -0300

    boludeces

---
+++ TP_AyD2_2024/src/interfaces/IStateServidor.java
@@ -0,0 +1,11 @@
+package interfaces;
+
+public interface IStateServidor {
+	
+	public void principal();
+	
+	public void secundario();
+	
+	public void accion();
+
+}
---
+++ TP_AyD2_2024/src/state/Secundario.java
@@ -0,0 +1,25 @@
+package state;
+
+import interfaces.IStateServidor;
+
+public class Secundario implements IStateServidor{
+
+	@Override
+	public void principal() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void secundario() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void accion() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
